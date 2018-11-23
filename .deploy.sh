99b4e026389e52da4a9f31df578657eef4287253
Petko D. Petkov <pdp@gnucitizen.org>
2018-11-23 20:23:55 +0000

    First.

---
+++ .deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp -f CNAME build/CNAME
+cd build
+git init
+git config user.name "Travis-CI"
+git config user.email "travis-ci@gnucitizen.org"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/opendevsecops/www" master:gh-pages > /dev/null 2>&1
---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/pownjs/www.svg?branch=master)](https://travis-ci.org/pownjs/www)
+[![Follow on Twitter](https://img.shields.io/twitter/follow/pownjs.svg?logo=twitter)](https://twitter.com/pownjs)
+
+# WWW
+
+The main site of OpenDevSecOps.
